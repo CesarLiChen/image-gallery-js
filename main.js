63802f3a43a2d8945776c3583b3e3ec1fc7e7ac8
Cesar Li Chen <cesarli619@gmail.com>
2023-06-03 01:08:38 -0400

    Some styling changes, and event listener added

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const current = document.querySelector("#current");
 const images = document.querySelector(".images");
 const imgArray = document.querySelectorAll(".images img"); // Array like node list
+const opacity = 0.6;
+
+// Opacity for first subimage
+imgArray[0].style.opacity = opacity;
+
+images.addEventListener("click", imageClick);
+
+function imageClick(event) {
+    console.log(event.target);
+
+    // Changes main image to clicked image.
+    current.src = event.target.src;
+}
