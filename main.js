89266259c12fd7e670e6aa04d90341739fe729d8
Cesar Li Chen <cesarli619@gmail.com>
2023-06-02 23:57:31 -0400

    Gets html elements with JS

---
+++ main.js
@@ -0,0 +1,3 @@
+const current = document.querySelector("#current");
+const images = document.querySelector(".images");
+const imgArray = document.querySelectorAll(".images img"); // Array like node list
